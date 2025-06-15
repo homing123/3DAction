2db4570fdf5aabca091c1971df21a0fda735697e
homing123 <86117180+homing123@users.noreply.github.com>
2025-06-15 09:25:17 +0900

    카메라 x축 회전 추가

--- Assets/3DAction/Script/PlayerCam.cs
+++ Assets/3DAction/Script/PlayerCam.cs
@@ -4,15 +4,18 @@ public class PlayerCam : MonoBehaviour
 {
     const string MouseY = "Mouse Y";
     const string MouseX = "Mouse X";
+    const float RotYClamp = 25;
     public static PlayerCam Ins;
     PlayerMove m_Player;
     float m_CurRotY;
+    float m_CurRotX;
 
     [SerializeField] float m_MouseRotSpeed = 200;
 
     [SerializeField] float m_Dis = 5;
     [SerializeField] float m_Height = 1;
     [SerializeField] float m_PlayerFocusHeight = 1.7f;
+    [SerializeField] bool m_CamRotActive;
 
     public float CamRotY { get { return m_CurRotY; } }
 
@@ -27,13 +30,24 @@ public class PlayerCam : MonoBehaviour
 
     void Start()
     {
+        m_CamRotActive = true;
         m_Player = PlayerMove.Ins;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetKeyDown(KeyCode.Q))
+        {
+            m_CamRotActive = !m_CamRotActive;
+        }
+        if(m_CamRotActive== false)
+        {
+            return;
+        }
         m_CurRotY += Input.GetAxis(MouseX) * Time.deltaTime * m_MouseRotSpeed;
+        m_CurRotX -= Input.GetAxis(MouseY) * Time.deltaTime * m_MouseRotSpeed;
+        m_CurRotX = Mathf.Clamp(m_CurRotX, -RotYClamp, RotYClamp);
         LookPlayer();
     }
 
@@ -49,7 +63,7 @@ public class PlayerCam : MonoBehaviour
         float disXZ = Mathf.Sqrt(m_Dis * m_Dis - m_Height * m_Height);
         Vector2 xzOffset = dirXZ * disXZ;
         transform.position = lookPos + new Vector3(-xzOffset.x, m_Height, -xzOffset.y);
-        transform.LookAt(lookPos);
+        transform.rotation = Quaternion.Euler(m_CurRotX, m_CurRotY, 0);
     }
    
 }

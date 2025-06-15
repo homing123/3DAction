653080db186dfc01c1897e472c9ab87631ed53bb
homing123 <86117180+homing123@users.noreply.github.com>
2025-06-15 22:28:29 +0900

    회전 자체감속 처리 적용

--- Assets/3DAction/Script/CarMove.cs
+++ Assets/3DAction/Script/CarMove.cs
@@ -3,6 +3,14 @@ using UnityEngine;
 
 public class CarMove : MonoBehaviour
 {
+    const float SelfDeceleration = 0.9f;
+
+    public enum CarAccType
+    {
+        None,
+        Forward,
+        Back
+    }
     public enum CarRotType
     {
         Straight,
@@ -12,11 +20,13 @@ public class CarMove : MonoBehaviour
 
     Rigidbody m_Rigid;
     CarRotType m_RotType = CarRotType.Straight;
+    CarAccType m_AccType = CarAccType.None;
     float m_CurSpeed;
     [SerializeField] float m_MaxSpeedForward;
     [SerializeField] float m_MaxSpeedBack;
     [SerializeField] float m_MaxAcc;
     [SerializeField] float m_RotAngle;
+
    
 
     private void Awake()
@@ -33,7 +43,11 @@ public class CarMove : MonoBehaviour
     {
         if (InputM.InputDir.y != 0)
         {
-            Acc(InputM.InputDir.y > 0);
+            Acc(InputM.InputDir.y > 0 ? CarAccType.Forward : CarAccType.Back);
+        }
+        else
+        {
+            Acc(CarAccType.None);
         }
         if (InputM.InputDir.x != 0)
         {
@@ -50,12 +64,49 @@ public class CarMove : MonoBehaviour
     }
     private void FixedUpdate()
     {
+        AccType();
         VelocityFixedUpdate();
     }
 
+    void AccType()
+    {
+        float acc = 0;
+        switch(m_AccType)
+        {
+            case CarAccType.None:
+                acc = -m_CurSpeed * SelfDeceleration;
+                break;
+            case CarAccType.Forward:
+                acc = Mathf.Clamp01((m_MaxSpeedForward - m_CurSpeed) / m_MaxSpeedForward);
+                acc *= m_MaxAcc;
+                //Debug.Log("전진 : "+((m_MaxSpeedForward - m_CurSpeed) / m_MaxSpeedForward )+ " " + acc);
+                break;
+            case CarAccType.Back:
+                acc = Mathf.Clamp01((m_CurSpeed + m_MaxSpeedBack) / m_MaxSpeedBack);
+                acc *= -m_MaxAcc;
+                //Debug.Log("후진 : " + ((m_CurSpeed - m_MaxSpeedBack) / m_MaxSpeedBack) + " " + acc);
+                break;
+        }
+      
+        m_CurSpeed += acc * Time.deltaTime;
+        m_CurSpeed = Mathf.Clamp(m_CurSpeed, -m_MaxSpeedBack, m_MaxSpeedForward);
+
+        if(m_AccType == CarAccType.None)
+        {
+            if (Mathf.Abs(m_CurSpeed) < 1f)
+            {
+                m_CurSpeed = 0;
+            }
+        }
+    }
     //감속 + 중력
     void VelocityFixedUpdate()
     {
+        if(m_CurSpeed == 0)
+        {
+            m_Rigid.linearVelocity = Vector3.zero;
+            return;
+        }
         float curRotAngle = 0;
         switch (m_RotType)
         {
@@ -68,30 +119,20 @@ public class CarMove : MonoBehaviour
                 curRotAngle = -m_RotAngle;
                 break;
         }
-        Vector3 forward = Quaternion.AngleAxis(-90 + curRotAngle, transform.up) * transform.forward;
-
+        float curMoveDis = m_CurSpeed * Time.fixedDeltaTime; //현재 물리프레임에 움직이는 거리
+        float rotAngle = curMoveDis * -curRotAngle;
+        Vector3 forward = Quaternion.AngleAxis(-90 + rotAngle, transform.up) * transform.forward;
+        m_Rigid.MoveRotation( Quaternion.AngleAxis(rotAngle, transform.up) * m_Rigid.rotation);
         m_Rigid.linearVelocity = forward * m_CurSpeed;
+        //회전감속
+        m_CurSpeed *= (90 - Mathf.Clamp(Mathf.Abs(rotAngle),0,90)) / 90;
     }
 
-    public void Acc(bool isForward)
+    public void Acc(CarAccType accType)
     {
         //전진의 경우 후진중일땐 가속도 최대값 0~최대전진속도 까지 Linear로 최대가속~0 
         //후진의 경우는 반대
-        float acc = 0;
-        if(isForward)
-        {
-            acc = Mathf.Clamp01((m_MaxSpeedForward - m_CurSpeed) / m_MaxSpeedForward);
-            acc *= m_MaxAcc;
-            //Debug.Log("전진 : "+((m_MaxSpeedForward - m_CurSpeed) / m_MaxSpeedForward )+ " " + acc);
-        }
-        else
-        {
-            acc = Mathf.Clamp01((m_CurSpeed + m_MaxSpeedBack) / m_MaxSpeedBack);
-            acc *= -m_MaxAcc;
-            //Debug.Log("후진 : " + ((m_CurSpeed - m_MaxSpeedBack) / m_MaxSpeedBack) + " " + acc);
-        }
-        m_CurSpeed += acc * Time.deltaTime;
-        m_CurSpeed = Mathf.Clamp(m_CurSpeed, -m_MaxSpeedBack, m_MaxSpeedForward);
+        m_AccType = accType;
     }
     public void Break()
     {
